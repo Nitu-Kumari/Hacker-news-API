20df4c7a9c1959c5132d5c7a4a8d93ef05d37e34
nitu <nituk650@gmail.com>
2019-01-28 01:06:57 +0530

    commit

--- MongodbService.js
+++ MongodbService.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 const dbName = "hacker";
-const url = "mongodb://Nitu:Delhi123@ds213645.mlab.com:13645/hacker";
+const url = "mongodb://nitu:Delhi123@ds213645.mlab.com:13645/hacker";
 
 class MongodbService {
   async init() {
